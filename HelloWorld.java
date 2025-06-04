61d2a6679f1acda22888d9e8c22bffeda31435d7
Nicolas-Cheron <150798236+MelineBes@users.noreply.github.com>
2025-06-04 12:20:22 +0200

    C1 ; ajout du prmeier fichier de code HelloWorld

---
+++ HelloWorld.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Hello world");
+        frame.setSize(200, 200);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JLabel label = new JLabel("Hello world", SwingConstants.CENTER);
+        label.setOpaque(true);
+        label.setBackground(Color.YELLOW);
+
+        frame.add(label);
+        frame.setVisible(true);
+    }
+}
+
