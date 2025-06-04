99c84a6cae4ada0d6a42cb52c22ff323da38f869
Nicolas-Cheron <150798236+MelineBes@users.noreply.github.com>
2025-06-04 12:39:20 +0200

    C3 : Mise à jour de la taille de la fenêtre et du texte des constantes

--- Constantes.java
+++ Constantes.java
@@ -1,4 +1,4 @@
 public class Constantes {
-    public static final String HELLO_WORLD_TITLE = "Title Hello world";
-    public static final String HELLO_WORLD_TEXT = "Hello world text";
+    public static final String HELLO_WORLD_TITLE = "Nouveau Title Hello world";
+    public static final String HELLO_WORLD_TEXT = "Nouveau Text Hello world";
 }
